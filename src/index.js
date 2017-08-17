14330ed675fa1f025951ef2f723f81296cbdeade
Di Wang <jackiewang5566@hotmail.com>
2017-08-16 20:55:15 -0400

    add input to SearchBar component and render it into index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 // Youtube API key: AIzaSyAM8-lc4kJQHwsACnbFNFAUjQ22Ld4-Wro
 const API_KEY = 'AIzaSyAM8-lc4kJQHwsACnbFNFAUjQ22Ld4-Wro';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
