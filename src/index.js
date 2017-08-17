8764e647301d33445ea248d1175ad5e5e3649c03
Di Wang <jackiewang5566@hotmail.com>
2017-08-16 20:40:23 -0400

    install youtube-api-search package and signup youtube-api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Youtube API key: AIzaSyAM8-lc4kJQHwsACnbFNFAUjQ22Ld4-Wro
+const API_KEY = 'AIzaSyAM8-lc4kJQHwsACnbFNFAUjQ22Ld4-Wro';
