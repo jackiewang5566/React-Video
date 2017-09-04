a720c652cca2b5364ea05898c8ce011fdc488b84
Di Wang <jackiewang5566@hotmail.com>
2017-09-03 23:34:41 -0400

    add style.css for global styling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import './style/style.css';
+
 const API_KEY = 'AIzaSyAM8-lc4kJQHwsACnbFNFAUjQ22Ld4-Wro';
 
 class App extends Component {
